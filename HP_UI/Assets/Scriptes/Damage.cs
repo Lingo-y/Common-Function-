321e029346c6d82217a81bd487b5470dc5e519ed
Lingo-y <78015560+Lingo-y@users.noreply.github.com>
2021-07-01 16:23:21 +0800

    Complete

---
+++ HP_UI/Assets/Scriptes/Damage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Damage : MonoBehaviour
+{
+    public float maxHP = 100;
+    public float currentHP=100;
+    private Image HP_Effect;
+    private Image HP_Point;
+    private Damage damage;
+
+    private void Awake()
+    {
+        currentHP = maxHP;
+        HP_Point = GameObject.Find("Canvas/HP_Point").GetComponent<Image>();
+        HP_Effect = GameObject.Find("Canvas/HP_Effect").GetComponent<Image>();
+       
+    }
+
+    IEnumerator Hurt()
+    {
+        HP_Point.fillAmount = currentHP / maxHP;
+        while (HP_Point.fillAmount <= HP_Effect.fillAmount)
+        {
+            HP_Effect.fillAmount -= 0.003f;
+            yield return new WaitForSeconds(0.005f);
+        }
+        if (HP_Point.fillAmount > HP_Effect.fillAmount)
+            HP_Effect.fillAmount = HP_Point.fillAmount;
+    }
+
+    public void Attack() {
+        if (currentHP <= 0)
+            return;
+        currentHP -= 10;
+        StartCoroutine(Hurt());
+    }
+}
---
+++ HP_UI/Assets/Scriptes/HP.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    private Image HP_Effect;
+    private Image HP_Point;
+    private Damage damage;
+
+    private void Awake()
+    {
+        HP_Point = GameObject.Find("Canvas/HP_Point").GetComponent<Image>();
+        HP_Effect = GameObject.Find("Canvas/HP_Effect").GetComponent<Image>();
+        damage = GameObject.Find("Canvas/Button_Damge").GetComponent<Damage>();
+        StartCoroutine(Hurt());
+    }
+
+    IEnumerator Hurt() {
+        HP_Point.fillAmount = damage.currentHP / damage.maxHP;
+        while (HP_Point.fillAmount<=HP_Effect.fillAmount) {
+            HP_Effect.fillAmount -= 0.003f;
+            yield return new WaitForSeconds(0.005f);
+        }
+        if (HP_Point.fillAmount > HP_Effect.fillAmount)
+            HP_Effect.fillAmount = HP_Point.fillAmount;
+    }
+    //private void Update()
+    //{
+    //    HP_Point.fillAmount = damage.currentHP / damage.maxHP;
+    //    if(HP_Point.fillAmount < HP_Effect.fillAmount)
+    //    {
+    //        HP_Effect.fillAmount -= 0.003f;
+    //    }
+    //    if (HP_Point.fillAmount > HP_Effect.fillAmount)
+    //        HP_Effect.fillAmount = HP_Point.fillAmount;
+    //}
+
+}
